233c07a812785e810259ba1942f56b1b8604c4ff
Harald Hoyer <harald@profian.com>
2022-05-12 09:03:39 +0200

    fix: correct nix flakes for reproducible builds
    Signed-off-by: Harald Hoyer <harald@profian.com>

--- flake.nix
+++ flake.nix
@@ -1,92 +1,120 @@
 {
   description = "Tools for deploying WebAssembly into Enarx Keeps.";
 
+  inputs.fenix.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.fenix.url = github:nix-community/fenix;
   inputs.flake-compat.flake = false;
   inputs.flake-compat.url = github:edolstra/flake-compat;
   inputs.flake-utils.url = github:numtide/flake-utils;
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-  inputs.fenix.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.fenix.url = github:rvolosatovs/fenix?ref=fix/rustc-patch;
-  inputs.naersk.url = github:nix-community/naersk;
-  inputs.naersk.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, fenix, flake-utils, naersk, ... }:
-    # NOTE: musl is only supported on Linux.
-    with flake-utils.lib; eachSystem [ system.x86_64-linux ] (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { self, nixpkgs, fenix, flake-utils, ... }:
+    with flake-utils.lib; eachSystem [
+      system.aarch64-darwin
+      system.aarch64-linux
+      system.x86_64-darwin
+      system.x86_64-linux
+    ]
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
 
-        rust = fenix.packages."${system}".fromToolchainFile {
-          file = "${self}/rust-toolchain.toml";
-        };
-      in
-      {
-        devShell = pkgs.mkShell.override { stdenv = pkgs.stdenvNoCC; } {
-          buildInputs = (with pkgs; [
-            gcc11
-            musl
-          ]) ++ [
-            rust
-          ];
+          cargoToml = builtins.fromTOML (builtins.readFile "${self}/Cargo.toml");
+        in
+        {
+          defaultPackage = self.packages.${system}.${cargoToml.package.name};
 
-          shellHook = ''
-            unset NIX_LDFLAGS_FOR_TARGET
-            unset NIX_CFLAGS_COMPILE_FOR_TARGET
-          '';
-        };
+          packages =
+            let
+              rust = with fenix.packages.${system};
+                combine [
+                  minimal.cargo
+                  minimal.rustc
+                  targets.wasm32-wasi.latest.rust-std # required for tests
+                  targets.x86_64-unknown-linux-musl.latest.rust-std
+                  targets.x86_64-unknown-none.latest.rust-std
+                ];
 
-        packages.enarx =
-          let
-            src = nixpkgs.lib.cleanSource self;
+              buildPackage = pkgs: extraArgs: (pkgs.makeRustPlatform {
+                rustc = rust;
+                cargo = rust;
+              }).buildRustPackage
+                (extraArgs // {
+                  inherit (cargoToml.package) name version;
 
-            # Common base derivation to build Enarx crates
-            buildEnarxPackage = { src, ... }@extraAttrs:
-              let
-                cargoToml = with builtins; fromTOML (readFile "${src}/Cargo.toml");
-                buildPackage = (naersk.lib.${system}.override {
-                  cargo = rust;
-                  rustc = rust;
-                }).buildPackage;
-              in
-              buildPackage ({
-                inherit src;
-                inherit (cargoToml.package) name version;
-              } // extraAttrs);
+                  src = pkgs.nix-gitignore.gitignoreRecursiveSource [
+                    "*.nix"
+                    "*.yml"
+                    "/.github"
+                    "/docs"
+                    "/README-DEBUG.md"
+                    "/SECURITY.md"
+                    "deny.toml"
+                    "flake.lock"
+                    "LICENSE"
+                    "rust-toolchain.toml"
+                  ]
+                    self;
 
-            # Enarx internal static dependencies
-            buildEnarxInternalPackage = src: buildEnarxPackage {
-              inherit src;
+                  cargoLock.lockFileContents = builtins.readFile "${self}/Cargo.lock";
 
-              CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+                  postPatch = ''
+                    patchShebangs ./helper
+                  '';
 
-              stripAllFlags = [ "--strip-unneeded" ];
-              stripAllList = [ "bin" ];
+                  buildInputs = pkgs.lib.optional pkgs.stdenv.isDarwin
+                    pkgs.darwin.apple_sdk.frameworks.Security;
+                });
+
+              dynamicBin = buildPackage pkgs {
+                cargoTestFlags = [ "wasm::" ];
+              };
+
+              staticBin = buildPackage pkgs.pkgsStatic
+                rec {
+                  CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+                  CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+                  CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER = with pkgs.pkgsMusl.stdenv; "${cc}/bin/${cc.targetPrefix}gcc";
+
+                  depsBuildBuild = [
+                    pkgs.stdenv.cc
+                  ];
+
+                  postBuild = ''
+                    ldd target/${CARGO_BUILD_TARGET}/release/${cargoToml.package.name} | grep -q 'statically linked' || (echo "binary is not statically linked"; exit 1)
+                  '';
+
+                  meta.mainProgram = cargoToml.package.name;
+                };
+
+              ociImage = pkgs.dockerTools.buildImage {
+                inherit (cargoToml.package) name;
+                tag = cargoToml.package.version;
+                contents = [
+                  staticBin
+                ];
+                config.Cmd = [ cargoToml.package.name ];
+                config.Env = [ "PATH=${staticBin}/bin" ];
+              };
+            in
+            {
+              "${cargoToml.package.name}" = dynamicBin;
+            } // pkgs.lib.optionalAttrs (system == flake-utils.lib.system.x86_64-linux) {
+              "${cargoToml.package.name}-static" = staticBin;
+              "${cargoToml.package.name}-docker" = ociImage;
             };
-            shimKvm = buildEnarxInternalPackage ./crates/shim-kvm;
-            shimSgx = buildEnarxInternalPackage ./crates/shim-sgx;
-            execWasmtime = buildEnarxInternalPackage ./crates/exec-wasmtime;
-          in
-          buildEnarxPackage {
-            inherit src;
 
-            ENARX_PREBUILT_shim-kvm = "${shimKvm}/bin/shim-kvm";
-            ENARX_PREBUILT_shim-sgx = "${shimSgx}/bin/shim-sgx";
-            ENARX_PREBUILT_exec-wasmtime = "${execWasmtime}/bin/exec-wasmtime";
-
-            CARGO_BUILD_TARGET = "x86_64-unknown-linux-gnu";
-
-            doCheck = true;
-            preCheck = ''
-              if [[ -e /dev/kvm ]]; then
-                export cargo_test_options="$cargo_test_options -- --skip check_listen_fd"
-              else
-                header "No KVM support, running only unit tests"
-                export cargo_test_options="$cargo_test_options --bins"
-              fi
-            '';
-          };
-
-        defaultPackage = self.packages.${system}.enarx;
-      }
-    );
+          devShell =
+            let
+              rust = fenix.packages.${system}.fromToolchainFile {
+                file = "${self}/rust-toolchain.toml";
+              };
+            in
+            pkgs.mkShell {
+              buildInputs = [
+                rust
+              ];
+            };
+        }
+      );
 }
